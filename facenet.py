121742847e9c5d39d08eb930195367b96d86759f
Samuel Tin <samuel.tin@gmail.com>
2018-03-20 00:23:12 +0800

    remove win specific code

--- facenet.py
+++ facenet.py
@@ -10,11 +10,11 @@ from numpy import genfromtxt
 import tensorflow as tf
 from fr_utils import *
 from inception_blocks_v2 import *
-import win32com.client as wincl
+#import win32com.client as wincl
 
 PADDING = 50
 ready_to_detect_identity = True
-windows10_voice_interface = wincl.Dispatch("SAPI.SpVoice")
+#windows10_voice_interface = wincl.Dispatch("SAPI.SpVoice")
 
 FRmodel = faceRecoModel(input_shape=(3, 96, 96))
 
@@ -185,7 +185,8 @@ def welcome_users(identities):
         welcome_message += 'and %s, ' % identities[-1]
         welcome_message += 'have a nice day!'
 
-    windows10_voice_interface.Speak(welcome_message)
+    #windows10_voice_interface.Speak(welcome_message)
+	print(welcome_message)
 
     # Allow the program to start detecting identities again
     ready_to_detect_identity = True
